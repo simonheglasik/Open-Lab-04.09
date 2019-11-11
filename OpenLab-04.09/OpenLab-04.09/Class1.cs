69267fe394ef02807df0f1f74401a73dd62154b8
simonheglasik <simonheglasik@gmail.com>
2019-11-11 09:43:56 +0100

    04.09

---
+++ OpenLab-04.09/OpenLab-04.09/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenLab_04._09
+{
+    class Class1
+    {
+        public List<string> RemoveDups(List<string> a)
+        {
+            for (int o = 0; o < a.Count; o++)
+            {
+                for (int i = 0; i < a.Count; i++)
+                {
+                    if (a[o] == a[i] & o != i)
+                    {
+                        a.Remove(a[o]);
+                    }
+                }
+            }
+            return a;
+        }
+    }
+}
---
+++ OpenLab-04.09/OpenLab-04.09/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System;
+namespace OpenLab_04._09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Class1();
+            List<string> list = new List<string> { "slovo", "slovo", "paprika", "Jozo", "paprika", "slovo" };
+            var lo = a.RemoveDups(list);
+            foreach (string s in lo) Console.WriteLine(s);
+        }
+    }
+}
